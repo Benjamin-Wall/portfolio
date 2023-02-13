18bc1f7726a5aeb3d8df01d5bacec151d537c52a
Benjamin Wall <benwallmain@outlook.com>
2023-02-13 13:23:04 +0000

    Made h1 title use reusable classname and updated GitHub disclaimer

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import "./projects.css";
 
 import { CardList } from "../CardList/CardList";
 
 export const Projects = () => {
   return (
     <>
-      <h1 id="projects">🔨 Projects: </h1>
+      <h1 className="heading">🔨 Projects: </h1>
       <p id="projects-desc">
         Below are some projects that i have completed along my path of Web
         Development <br />
-        (This part uses the GitHub API to get all my public repositories!)
+        (This part uses the GitHub API to get all my repositories marked with
+        the tag "show-on-website")
       </p>
       <CardList />
     </>
